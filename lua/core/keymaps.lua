3b00bbbe0fcc0c665fb0a6e03f6e87acfba524f2
David <david.haland@bouvet.no>
2023-09-14 15:32:24 +0200

    init

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,21 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<Space>", "<Nop>")
+
+-- Move lines in visaul mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Remap to clipboard
+vim.keymap.set({ "n", "v" }, "<leader>p", [["*p]], { desc = "Paste from clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>y", [["*y]], { desc = "Yank to clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>d", [["*d]], { desc = "Cut to clipboard" })
+
+-- Text editing
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace text" })
+
+-- Navigation
+vim.keymap.set("n", "<C-o>", "<C-o>zz")
+vim.keymap.set("n", "<C-i>", "<C-i>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
